7effd6a738e840aa7c56cbf8f1dce14e59136a18
kennethihezie <kennethihezie06@gmail.com>
2024-05-04 13:35:57 +0100

    moved app inside module folder

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { ValidationPipe } from '@nestjs/common';
-import { AppModule } from './app/app.module';
+import { AppModule } from './modules/app/app.module';
 import { NestFactory } from '@nestjs/core';
 
 
